b300ab0afe3822855c7a72eb60076b728690ab74
sagar9322 <sagarkasundra223@gmail.com>
2023-05-09 23:58:43 +0530

    deleteFromLocalStorage

--- booking.js
+++ booking.js
@@ -6,8 +6,7 @@ var count = localStorage.length+1;
     
       });
 
-
-
+    
 function printUserDetail(event) {
     event.preventDefault();
     // Get form input values
@@ -16,10 +15,53 @@ function printUserDetail(event) {
     const phone = document.getElementById("phone").value;
     const date = document.getElementById("date").value;
     const time = document.getElementById("time").value;
-    let userDetail = {Name:name,Email:email,Phone:phone,Date:date,Time:time};
-let stringUserDetail = JSON.stringify(userDetail);
-    // adding in localstorage
-    localStorage.setItem("Candidate-"+count, stringUserDetail);
+
+    // converting userdetail into object
+    var userDetail = {Name:name,Email:email,Phone:phone,Date:date,Time:time};
+
+
+    // converting object to string
+    let stringUserDetail = JSON.stringify(userDetail);
+
+    // making addevent and remove event 
+    var itemList = document.getElementById('items');
+    itemList.addEventListener('click', removeItem);
+
+    //making new element li
+    var li = document.createElement('li');
+    // Add class
+    li.className = 'list-group-item';
+    // Add text node with input value
+    li.appendChild(document.createTextNode(JSON.stringify(userDetail)));
+  
+    // Create del button element
+    var deleteBtn = document.createElement('button');
+  
+    // Add classes to del button
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  
+    // Append text node
+    deleteBtn.appendChild(document.createTextNode('Delete'));
+  
+    // Append button to li
+    li.appendChild(deleteBtn);
+    
+  
+    // Append li to list
+    itemList.appendChild(li);
+
+    // remove li function
+    function removeItem(e){
+      if(e.target.classList.contains('delete')){
+        var li = e.target.parentElement;
+          var email = JSON.parse(li.firstChild.textContent).Email;
+          
+          itemList.removeChild(li);
+          localStorage.removeItem(`${email}`);
+          
+      }
+    }
+    localStorage.setItem(email, stringUserDetail);
     document.getElementById("fname").value = "";
   document.getElementById("email").value = "";
   document.getElementById("phone").value = "";
