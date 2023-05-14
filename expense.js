c9f1e46d5f26aec252744e1a7a9ffe28868b86af
sagar9322 <sagarkasundra223@gmail.com>
2023-05-14 11:28:21 +0530

    modified-screen-item

--- expense.js
+++ expense.js
@@ -7,21 +7,20 @@ function printExpenseDetail(event) {
 
     // converting userdetail into object
     var userDetail = {Amount:amount,Description:description,Category:category};
+    var displayDetail = userDetail.Amount + '-' + userDetail.Description + '-' + userDetail.Category;
 
     // converting object to string
     let stringUserDetail = JSON.stringify(userDetail);
 
     // making addevent and remove event 
     var itemList = document.getElementById('items');
-    itemList.addEventListener('click', removeItem);
-    // itemList.addEventListener('dblclick', editDetails);
 
     //making new element li
     var li = document.createElement('li');
     // Add class
     li.className = 'list-group-item';
     // Add text node with input value
-    li.appendChild(document.createTextNode(stringUserDetail));
+    li.appendChild(document.createTextNode(displayDetail));
   
     // Create del button element
     var deleteBtn = document.createElement('button');
@@ -34,38 +33,28 @@ function printExpenseDetail(event) {
     // Append text node
     deleteBtn.appendChild(document.createTextNode('Delete'));
     editBtn.appendChild(document.createTextNode('Edit'));
+
+    deleteBtn.onclick = () => {
+      localStorage.removeItem(userDetail.Description);
+      itemList.removeChild(li);
+    }
+    editBtn.onclick = () => {
+      localStorage.removeItem(userDetail.Description);
+      itemList.removeChild(li);
+      document.getElementById("amount").value = amount;
+  document.getElementById("description").value = description;
+  document.getElementById("select").value = category;
+    }
   
     // Append button to li
     li.appendChild(deleteBtn);
     li.appendChild(editBtn);
+
     
   
     // Append li to list
     itemList.appendChild(li);
 
-    // remove li function
-    function removeItem(event){
-      event.preventDefault();
-      
-      if(event.target.classList.contains('delete')){
-        var li = event.target.parentElement;
-          var description = JSON.parse(li.firstChild.textContent).Description;
-          
-          itemList.removeChild(li);
-          localStorage.removeItem(`${description}`);
-          
-      }
-      else if(event.target.classList.contains('edit')){
-        var li = event.target.parentElement;
-          var description = JSON.parse(li.firstChild.textContent).Description;
-          
-          itemList.removeChild(li);
-          localStorage.removeItem(`${description}`);
-          document.getElementById("amount").value = amount;
-  document.getElementById("description").value = description;
-  document.getElementById("select").value = category;
-      }
-    }
     localStorage.setItem(description, stringUserDetail);
     document.getElementById("amount").value = "";
   document.getElementById("description").value = "";
